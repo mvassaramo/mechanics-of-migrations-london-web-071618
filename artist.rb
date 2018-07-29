1682c2b040531cb98773579773d01cb2a2a8beee
github username <github email address>
2018-07-29 17:29:26 +0100

    Done.

---
+++ artist.rb
@@ -0,0 +1,15 @@
+class Artist < ActiveRecord::Base 
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
---
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -0,0 +1,10 @@
+class AddFavoriteFoodToArtists < ActiveRecord::Migration
+
+
+
+  def change
+      add_column :artists, :favorite_food, :string
+  end
+
+
+end
